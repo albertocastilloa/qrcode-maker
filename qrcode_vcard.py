e1a4aba5d66ea664fd32da422dd901c15b39bc89
Alberto Castillo <49110192+albertocastilloa@users.noreply.github.com>
2020-06-12 09:55:29 -0500

    Update qrcode_vcard.py

--- qrcode_vcard.py
+++ qrcode_vcard.py
@@ -1,6 +1,15 @@
+#Import segno library
 from segno import helpers
 
 def get_full_qrcode(n, ln, e, u, p):
+    """
+    It will return a .svg QR Code file
+    n = First Name
+    l = Last Name
+    e = E-mail
+    u = URL. Do not use http:// or https://. Only your domain name, example: mydomain.com
+    p = phone
+    """
     qr = helpers.make_mecard(name=ln+","+n, email=e, url=u, phone=p)
     qr.save(str(n)+"_"+str(ln)+".png", scale=10, dark="darkblue")
 
@@ -10,4 +19,5 @@ e = "albertocastilloa@me.com"
 u = "github.com/albertocastilloa"
 p = "5512345678"
 
+#Call the function
 get_full_qrcode(n, ln, e, u, p)
